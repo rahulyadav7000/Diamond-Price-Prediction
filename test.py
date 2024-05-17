ca1eca81faa03234b56989d2f85a720bc4f7040f
rahul yadav <rahul.yadavv7000@gmail.com>
2024-05-17 15:24:05 +0530

    app.py and template created

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+"""
+
+
 import os
 
 path="notebooks/research.ipynb"
@@ -8,3 +11,13 @@ os.makedirs(dir,exist_ok=True)
 
 with open(path,"w") as f:
     pass
+
+"""
+from src.DimondPricePrediction.pipelines.prediction_pipeline import CustomData
+
+
+custdataobj=CustomData(1.52,62.2,58.0,7.27,7.33,4.55,"Premium","F","VS2")
+
+data=custdataobj.get_data_as_dataframe()
+
+print(data)
